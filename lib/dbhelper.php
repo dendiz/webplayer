5427df55cdbae5a59407c1d105cfa766bb590396
Deniz Dizman <deniz.dizman@rocket-internet.com.tr>
2012-08-08 10:44:00 +0300

    adding some helpers

---
+++ lib/dbhelper.php
@@ -0,0 +1,45 @@
+<?php
+require_once(dirname(__FILE__).'/../lib/utils.php');
+function db() {
+	_log('db host', DB_HOST, 'db user', DB_USER, 'db pass', DB_PASS, 'db name',DB_NAME);
+	mysql_connect(DB_HOST,DB_USER,DB_PASS) or die('cannot connect db');
+	mysql_select_db(DB_NAME) or die('cannot select db: '.mysql_errno()." ".mysql_error());
+}
+function fetch_all($res) {
+	$rows = Array();
+	while( ($row = mysql_fetch_assoc($res)) != null) {
+		$rows[] = $row;
+	}
+	return $rows;
+}
+function insert() {
+	$args = func_get_args();
+	$sql_tmp = array_shift($args);
+	$args = array_map('mysql_real_escape_string', $args);
+	_log('insert() args after shift:', var_export($args,true));
+	$sql = vsprintf($sql_tmp, $args); 
+	_log('sql statement -',$sql);
+	$res = mysql_query($sql);
+	_log('result of the statement was -', var_export($res,true));
+	return $res;
+}
+function query($sql) {
+	_log('sql - ', $sql);
+	$res = mysql_query($sql);
+	if (!$res) _log('mysql error no: '.mysql_errno($res). ' message: '.mysql_error($res));
+	return $res;
+}
+function query_one($sql) {
+	$res = query($sql);
+	return fetch_one($res);
+}
+function query_all($sql) {
+	$res = query($sql);
+	return fetch_all($res);
+}
+function fetch_one($res) {
+	return fetch_row($res);
+}
+function fetch_row($res) {
+	return mysql_fetch_assoc($res);
+}
---
+++ lib/utils.php
@@ -0,0 +1,68 @@
+<?php
+function _human_time($timestamp) {
+	$difference = time() - $timestamp;
+	$periods = array("sec", "min", "hour", "day", "week",
+	"month", "years", "decade");
+	$lengths = array("60","60","24","7","4.35","12","10");
+
+	if ($difference > 0) { // this was in the past
+	$ending = "ago";
+	} else { // this was in the future
+	$difference = -$difference;
+	$ending = "to go";
+	}
+	for($j = 0; $difference >= $lengths[$j]; $j++)
+	$difference /= $lengths[$j];
+	$difference = round($difference);
+	if($difference != 1) $periods[$j].= "s";
+	$text = "$difference $periods[$j] $ending";
+	return $text;
+}
+function _slug($text) {
+    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    $text = trim($text, '-');
+    if (function_exists('iconv'))
+    {
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+    $text = strtolower($text);
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    if (empty($text))
+    {
+        return 'n-a';
+    }
+    return (strlen($text) > 50) ? substr($text, 0,50):$text;
+}
+function _ellipsis($text, $th) {
+	if (strlen($text) > $th) return substr($text, 0, $th-3)."...";
+	return $text;
+}
+function _rand_url() {
+	return md5(time()).md5(rand());
+}
+function _post($key, $default = false) {
+	return isset($_POST[$key]) ? $_POST[$key] : $default;
+}
+function _get($key, $default = false) {
+	return isset($_GET[$key]) ? $_GET[$key] : $default;
+}
+function _error($code, $msg) {
+	return json((array("error_code" => $code, "user_message" => $msg)));
+}
+
+function _log() {
+	$msg = implode(' ', func_get_args());
+	$msg = date("H:i:s") . " - " . $msg . PHP_EOL;
+	file_put_contents("/tmp/webplayer.log", $msg, FILE_APPEND);
+}
+function _glob_recursive($pattern, $flags = 0)
+{
+	$files = glob($pattern, $flags);
+	
+	foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+	{
+		$files = array_merge($files, _glob_recursive($dir.'/'.basename($pattern), $flags));
+	}
+	
+	return $files;
+}
